ab529394ee1d0679b423025734dffd2bc2a69e31
Manik Lakhanpal <lakhanpalmanik@gmail.com>
2024-01-27 11:55:48 +0530

    added joke api

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-import express from "express";
+import express, { response } from "express";
+import axios from "axios";
 import fs from "fs";
+import { title } from "process";
 
 const app = express();
 const port = 7777;
@@ -29,10 +31,21 @@ let jsonArray = JSON.parse(json);
 app.use(logger);
 app.use(express.static('public'));
 
-app.get('/home', (req, res) => {
+app.get('/home', async (req, res) => {
+    const response = await axios.get("https://v2.jokeapi.dev/joke/Any");
+    const result = response.data;
+    try {
+        res.render("home.ejs", {
+            title: "Home",
+            joke: JSON.stringify(result.joke),
+        })
+    }
+    catch (error) {
     res.render("home.ejs", {
         title: "Home",
+        joke: JSON.stringify(result.delivery),
     })
+}
 })
 
 app.get('/signin', (req, res) => {
