6a93e53a2d52c10515759c207c0c0a12523f3bab
Manik Lakhanpal <lakhanpalmanik@gmail.com>
2024-12-18 22:12:53 +0530

    gsap added on opening

--- frontend/src/containers/homePage.tsx
+++ frontend/src/containers/homePage.tsx
@@ -20,6 +20,13 @@ export default function HomePage() {
       duration: 2,
       ease: "power4.out",
     });
+
+    gsap.from(content.current, {
+      x: "0%",
+      opacity: "0",
+      duration: 5,
+      ease: "power4.out"
+    })
   }, []);
 
   return (
@@ -33,12 +40,13 @@ export default function HomePage() {
             >
               Welcome to <span className="text-blue-400">Blog</span>
             </h1>
-            <div>
+            
+            <div ref={content}>
               <p className="text-xl sm:text-2xl text-gray-300 max-w-2xl mx-auto">
                 A professional blog platform featuring integrated messaging
                 capabilities (currently under development)
               </p>
-              <p className="text-xl sm:text-2xl text-gray-300 max-w-2xl mx-auto">
+              <p className="text-xl sm:text-2xl text-gray-300 max-w-2xl mx-auto mt-5">
                 Created by Manik Lakhanpal
               </p>
             </div>
@@ -47,38 +55,38 @@ export default function HomePage() {
                 <SiDeno
                   className="text-3xl text-white deno"
                   title="Deno"
-                  onClick={contextSafe(() => {
+                  onClick={() => {
                     gsap.to(".deno", { rotation: "+=180" });
-                  })}
+                  }}
                 />
 
                 <RiNextjsFill
                   className="text-4xl text-white react"
                   title="React (Next.js)"
-                  onClick={contextSafe(() => {
+                  onClick={() => {
                     gsap.to(".react", { rotation: "+=180" });
-                  })}
+                  }}
                 />
                 <FaDatabase
                   className="text-3xl text-green-500 mongo"
                   title="MongoDB"
-                  onClick={contextSafe(() => {
+                  onClick={() => {
                     gsap.to(".mongo", { rotation: "+=180" });
-                  })}
+                  }}
                 />
                 <SiSocketdotio
                   className="text-3xl text-yellow-400 socket"
                   title="Socket.io"
-                  onClick={contextSafe(() => {
+                  onClick={() => {
                     gsap.to(".socket", { rotation: "+=180" });
-                  })}
+                  }}
                 />
                 <FaCloud
                   className="text-3xl text-purple-400 cloud"
                   title="Cloud Deployed"
-                  onClick={contextSafe(() => {
+                  onClick={() => {
                     gsap.to(".cloud", { rotation: "+=180" });
-                  })}
+                  }}
                 />
               </div>
             </div>
