3924eba89b05c003bec57bbd3b7039075047be26
Manik Lakhanpal <lakhanpalmanik@gmail.com>
2024-12-29 20:23:44 +0530

    Seperated technologies from techStack.tsx

--- frontend/src/containers/techStack.tsx
+++ frontend/src/containers/techStack.tsx
@@ -1,34 +1,13 @@
 import {GradualSpacing} from "@/components/ui/gradual-spacing";
 import Image from "next/image";
 import Particles from "@/components/ui/particles";
+import technologies from "@/data/technologies.json"
 
 interface Technology {
     name: string;
     icon: string;
     color: string;
 }
-
-const technologies: Technology[] = [
-    // Frontend technologies
-    { name: 'React', icon: '/tech/react.svg', color: 'text-[#61DAFB]' },
-    { name: 'NextJs', icon: '/tech/nextjs.svg', color: 'text-[#a6a6a6]' },
-    { name: 'JavaScript', icon: '/tech/javascript.svg', color: 'text-[#F7DF1E]' },
-    { name: 'TypeScript', icon: '/tech/typescript.svg', color: 'text-[#3178C6]' },
-    { name: 'Tailwind', icon: '/tech/tailwind.svg', color: 'text-[#38B2AC]' },
-
-    // Backend technologies
-    { name: 'Node.js', icon: '/tech/nodejs.svg', color: 'text-[#339933]' },
-    { name: 'Express.js', icon: '/tech/express.svg', color: 'text-[#000000]' },
-    { name: 'Deno', icon: '/tech/deno.svg', color: 'text-[#ffffff]' },
-    { name: 'Digital Ocean', icon: '/tech/digitalocean.svg', color: 'text-[#0058F5]' },
-
-    // Databases
-    { name: 'MongoDB', icon: '/tech/mongodb.svg', color: 'text-[#47A248]' },
-
-    // Tools and platforms
-    { name: 'Docker', icon: '/tech/docker.svg', color: 'text-[#3178C6]' },
-    { name: 'Nginx', icon: '/tech/nginx.svg', color: 'text-[#339933]' },
-];
 
 export default function TechStack() {
     return (
